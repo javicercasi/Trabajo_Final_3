b09c21c76a204443e0ab0c47bc040659097bf727
javicercasi <jacercasii@gmail.com>
2022-06-10 11:01:32 -0300

    Avances

--- 58004-Cercasi/Backend/Auxiliares/unif4_anda/unif4_anda.ino
+++ 58004-Cercasi/Backend/Auxiliares/unif4_anda/unif4_anda.ino
@@ -4,6 +4,8 @@
 #include <String.h>
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
+
+
 int i=0, j=0;
 Separador s;
 unsigned long previo, actual, a, duracion;
@@ -16,7 +18,7 @@ unsigned long tiempo_previo = 0, transcurrido = 0;
 const long intervalo = 3000;
 
 char keys[rowsCount][columsCount] = {
-   { '1','2','3', 'A' },
+   { '1','2','3', '+' },
    { '4','5','6', 'B' },
    { '7','8','9', 'C' },
    { '*','0','#', 'D' }
@@ -66,6 +68,8 @@ void Configuracion(){
   
   comando("AT+CLIP=1", 500);    //Identificador de llamda activado
   comando("AT+IFC=1,1", 500);   //Retorno de datos por Soft
+  comando("AT+CMGF=1", 1000);
+  //comando("AT+CPBF", 4000); 
   //comando("AT+CMIC=0,7", 500);  //Ganancia microfono 0-15
   //comando("AT+CALS=4", 500);    //Tono seleccionado 0-19
   //comando("AT+CLVL=80", 500);   //Volumen parlante 0-100
@@ -94,9 +98,15 @@ void llamado(String posicion){
       }
    }
    String numero = s.separa(data, '"', 1);
+   lcd.clear();
+   lcd.setCursor(0, 0);
+   lcd.print("   Llamando a:  ");
+   lcd.setCursor(0, 1);
+   lcd.print(" "+numero+"   ");
    Serial.println("LLamando a "+numero);
-
    comando("ATD"+numero+";", 500);
+   delay(6000);
+   timmer(posicion);
 }
 
 
@@ -104,6 +114,7 @@ void oyente(){
 
   #define DEBUG(a) Serial.println(a);
   String data = "";
+  int posicion = 8;
   Serial1.println("AT+CPAS");
   delay(1300);
     while (Serial1.available())
@@ -114,34 +125,40 @@ void oyente(){
          data.concat(character);
       }
    }
-
    teclado();
    String numero = s.separa(data, ':', 1);
    byte prevPos = numero.indexOf('O');                       // Buscar la posición de la primera coma en la cadena
    String first = numero.substring(0, prevPos);
    String numero2 = s.separa(data, '"', 1);
-   
+   String numero4 = s.separa(data, '"', 5);
+   //Serial.println("numero entero: "+numero+ " Contacto que llama: "+ numero4 + "  O tambien: " + String(posicion) + " Tamaño es: "+ numero4.length());
+   //delay(100);
+
+   if (numero4.length() > 0){
+      posicion = numero4.toInt();
+      if (posicion > 7){
+        posicion = 8;}
+        Serial.println("Contacto que llama: "+ numero4 + "  O tambien: " + String(posicion) + " Tamaño es: "+ numero4.length());}
+    
    if (numero2.length() == 0){
       i = 0;
       j = 0;
       numero2 = "";}
       
-   if (numero2.length() == 13 && i == 0){
+   if (numero2.length() == 13 && i == 0 && numero.length() > 22){
       char tecla = keypad.getKey();
-      i++;    //Aca debo detectar el ata
+      i++;   
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Llamada Entrante:");
       lcd.setCursor(0, 1);
       lcd.print(numero2);
-
-      //Serial.println("Llamada de: "+numero2);
       
       while (tecla != '*' && tecla != '#'){
                 tecla = keypad.getKey();
                 if (tecla == '*') {
                   Serial1.println("ATA");
-                  timmer();
+                  timmer(String(posicion));
                                   }
                   
                  if (tecla == '#') {
@@ -152,6 +169,7 @@ void oyente(){
                   lcd.setCursor(0, 1);
                   lcd.print("   Rechazada   ");
                   delay(4500);
+                  //posicion = 0;
                   standby();
     }}
       }
@@ -173,22 +191,27 @@ void oyente(){
         oyente();}
         
               }
-void timmer()
+void timmer(String posicion)
   {
-  boolean a = true;
+
+  #define DEBUG(a) Serial.println(a);
+  String data = "";
+  boolean b = true, c = true;
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Llamada:");
   previo = millis()/1000;
   Serial.print( "Pase" );
-  while (a){
+  while (b){
+
     char key = keypad.getKey();
     lcd.setCursor(0, 1);
     lcd.print(String(millis()/1000 - previo) + " Segundos");
     delay(100);
-    if (key == '#'){
+    
+    if (key == '#' || c == false){
         Serial1.println("ATH");
-        a = false;
+        b = false;
         duracion = millis()/1000 - previo;
         lcd.clear();
         lcd.print("Fin de Llamada");
@@ -196,6 +219,7 @@ void timmer()
         lcd.print("Duracion: " + String(duracion) + " s.");
         delay(4500);
         standby();
+        //envio_trama(posicion, String(duracion));
         }
   }
 }
@@ -226,53 +250,84 @@ void teclado(){
               Serial.println("Su apikey es: "+apikey);}
       }
     
-    if (key == '*'){
+    if (key == 'B'){
+        lcd.clear();
+        lcd.print("Modulo Encendido");
+        delay(3000);
         Serial.println("Encendido de Modulo");
-        encendido();}
+        encendido();
+        standby();}
     
-    if (key == 'A'){
+    if (key == '#'){
+        lcd.clear();
+        lcd.print("Reset de Modulo");
+        delay(3000);
         Serial.println("Reseteado del Modulo");
-        reset();}
+        reset();
+        standby();}
 
     if (key == '#'){
         Serial.println("Fin llamada");}
 
-     if (key == 'C'){
+     if (key == 'D'){
+              lcd.clear();
+              lcd.print("  Modo Registro: ");
+              delay(4000);
+              lcd.setCursor(0, 0);
+              lcd.print("Ingrese numero: ");
+              lcd.setCursor(0, 1);
               Serial.println("Modo Registro");
               String num = "", posicion = "";
               Serial.println("Ingrese el numero de Telefono: ");
 
-              
               for(int i = 1; i<3; i++){
               if (i == 2){Serial.println();
+                          lcd.clear();
+                          lcd.print("Ingrese Posicion");
+                          lcd.setCursor(0, 1);
+                          lcd.print("a Guardar: ");
                           Serial.println("Ingrese la posicion a guardarse: ");}
               char tecla = keypad.getKey();
-              while (tecla != '#'){
+              while (tecla != 'D'){
                 tecla = keypad.getKey();
                 if ((tecla != NO_KEY) && (tecla != '#') && (tecla != '*') && (tecla != 'A') && (tecla != 'B') && (tecla != 'C') && (tecla != 'D')) {
                   if (i == 1){num.concat(tecla);}
                   if (i == 2){posicion.concat(tecla);}
                   Serial.write(tecla);
-                  //lcd.print(keyPressed);
+                  lcd.print(tecla);
                   }   
                 }}
  
-            String comando =  "\tAT+CPBW="+String(posicion)+",\""+String(num)+"\",129,\"Contacto\"";
+            String comandoat =  "\tAT+CPBW="+String(posicion)+",\""+String(num)+"\",129,\""+posicion+"\"";
             //Serial1.println(comando);
-            Serial.println("Comando:  "+comando);
+            lcd.clear();
+            comando(comandoat, 1000);
+            Serial.println("Comando:  "+comandoat);
+            lcd.print("Numero Guardado");
+            delay(3000);
+            standby();
         }
-            if (key == 'D'){
+            if (key == 'C'){
+              lcd.clear();
+              lcd.print("  Modo Llamada  ");
+              delay(4000);
+              lcd.setCursor(0, 0);
+              lcd.print("Ingrese Contacto");
+              lcd.setCursor(0, 1);
+              lcd.print("a Llamar: ");
               Serial.println("Modo Llamada");
               Serial.println("Ingrese la posicion a Leerse: ");
               String num = "";
               boolean a = true;
               char tecla = keypad.getKey();
-              while (tecla != '#'){
+              while (tecla != '*'){
                 tecla = keypad.getKey();
-                if ((tecla != NO_KEY) && (tecla != '#') && (tecla != '*') && (tecla != 'A') && (tecla != 'B') && (tecla != 'C') && (tecla != 'D')) {
+                if ((tecla != NO_KEY) && (tecla != '#') && (tecla != '*') && (tecla != '+') && (tecla != 'B') && (tecla != 'C') && (tecla != 'D')) {
                   num.concat(tecla);
                   Serial.println();
                   Serial.write(tecla);
+                  lcd.print(tecla);
+;
                   }}
               int b = num.length();
               Serial.println("Numero: "+ num +"    Extension teclas: "+ String(b));
@@ -282,6 +337,112 @@ void teclado(){
               Serial.println("LLamando funcion");
               llamado(num);}
             }
-        
+
+
+            if (key == '+'){
+              lcd.clear();
+              lcd.print("  Modo Mensaje: ");
+              delay(4000);
+              lcd.setCursor(0, 0);
+              lcd.print("Ingrese Mensaje");
+              lcd.setCursor(0, 1);
+              lcd.print("preguardado: ");
+              Serial.println("Modo Mensaje");
+              String num = "", posicion = "";
+              Serial.println("Ingrese el mensaje preguardado: ");
+
+              for(int i = 1; i<3; i++){
+              if (i == 2){Serial.println();
+                          lcd.clear();
+                          lcd.print("Ingrese Contacto");
+                          lcd.setCursor(0, 1);
+                          lcd.print("a Enviar: ");
+                          Serial.println("Ingrese la posicion a guardarse: ");}
+              char tecla = keypad.getKey();
+              while (tecla != '+'){
+                tecla = keypad.getKey();
+                if ((tecla != NO_KEY) && (tecla != '#') && (tecla != '*') && (tecla != '+') && (tecla != 'B') && (tecla != 'C') && (tecla != 'D')) {
+                  if (i == 1){num.concat(tecla);}
+                  if (i == 2){posicion.concat(tecla);}
+                  Serial.write(tecla);
+                  lcd.print(tecla);
+                  }   
+                }}
+            mensaje(num, posicion);
+        }
         } 
+  }
+
+
+
+  void mensaje(String num, String posicion){
+
+      #define DEBUG(a) Serial.println(a);
+      String data = "", texto = "";
+      Serial1.println("AT+CPBR="+posicion);
+      delay(2000);
+        while (Serial1.available())
+      {
+        char character = Serial1.read();
+        if (character != '\n')
+        {
+           data.concat(character);
+        }
+      }
+      String numero = s.separa(data, '"', 1);
+  
+      switch (num.toInt()) {
+      case (1):
+        texto = "Hola";
+        break;
+      case (2):
+        texto = "Estoy bien";
+        break;
+      case (3):
+        texto = "Llamame";
+        break;
+      case (4):
+        texto = "Ven a Verme";
+        break;
+      default:
+       texto = "Hola";
+       break;}
+
+      String comandoat = "AT+CMGS=\"" + numero + "\"\r\n";
+      comando(comandoat, 1000);
+      Serial.println(comandoat);
+      comando(texto,1000);
+      Serial1.println((char)26); 
+      delay(1000);
+      lcd.clear();
+      lcd.print("Mensaje Enviado:");
+      lcd.setCursor(0, 1);
+      lcd.print(texto);
+      Serial.println("Mensaje enviado");
+      delay(3000);
+      standby();
+
+    }
+
+  void envio_trama(String posicion, String duracion){
+
+   Serial.println("La posicion es: "+ String(posicion.toInt()));
+
+  if (posicion.toInt() > 7){
+    posicion = "8";}
+
+  comando("AT+CIPSTATUS", 2000);
+  comando("AT+CIPMUX=0", 2000);
+  comando("AT+CSTT=\"datos.personal.com\",\"datos\",\"datos\"", 1000);
+  comando("AT+CIICR", 3000);
+  comando("AT+CIFSR", 2000);
+  comando("AT+CIPSPRT=0", 3000);
+  comando("AT+CIPSTART=\"TCP\",\"184.106.153.149\",\"80\"", 6000);
+  comando("AT+CIPSEND", 4000);
+  String datos="GET https://184.106.153.149/update?api_key=96MTTXG33GMWMI2Q&field"+ posicion + "="+ duracion;
+  comando(datos, 4000);
+  Serial1.println((char)26);
+  delay(5000);
+  comando("",500);
+  comando("AT+CIPSHUT",4000);
   }
